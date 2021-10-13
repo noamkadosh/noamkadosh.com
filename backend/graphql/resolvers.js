181718c935b450327aa92487dd5ab0a13ecbc503
Noam Kadosh <noamkadosh91@gmail.com>
2021-10-12 18:23:43 -0700

    Disable signup.

--- backend/graphql/resolvers.js
+++ backend/graphql/resolvers.js
@@ -5,57 +5,66 @@ const validator = require('validator');
 const jwt_config = require('../config').jwt;
 const User = require('../models/user');
 
+const signupDisabled = true;
+
 module.exports = {
   signup: ({ userInput }, request) => {
-    // Validation
-    const errors = [];
-    if (!validator.isEmail(userInput.email)) {
-      errors.push({ msg: 'Email invalid.' });
-    }
-    if (
-      validator.isEmpty(userInput.password) ||
-      !validator.isLength(userInput.password, { min: 6 })
-    ) {
-      errors.push({ msg: 'Password too short!' });
-    }
-    if (userInput.password !== userInput.passwordConfirmation) {
-      errors.push({ msg: 'Passwords do not match.'});
-    }
-    if (errors.length > 0) {
-      const error =  new Error('Invalid input.');
-        error.status = 422;
-        error.msg = 'Invalid input.';
-        error.data = errors;
-        throw(error);
-    }
-    // Handling data
-    return bcrypt.hash(userInput.password, 12)
-        .then(hash => {
-            const user = new User({
-                email: userInput.email,
-                password: hash
-            });
-            return user.save()
-                .then(createdUser => {
-                    const { __v, ...createdUserData } = createdUser._doc;
-                    return {
-                      ...createdUserData,
-                      _id: createdUser._id.toString()
-                    };
-                })
-                .catch(err => {
-                    const error =  new Error(err);
-                    error.status = 400;
-                    error.msg = 'Unknown error occured.';
-                    throw(error);
-                });
-        })
-        .catch(err => {
-            const error =  new Error(err);
-            error.status = 400;
-            error.msg = 'Unknown error occured.';
-            throw(error);
-        });
+    if (signupDisabled) {
+      const error =  new Error('Admin disabled signup option.');
+      error.status = 403;
+      error.msg = 'Admin disabled signup option.';
+      throw(error);
+    } else {
+      // Validation
+      const errors = [];
+      if (!validator.isEmail(userInput.email)) {
+        errors.push({ msg: 'Email invalid.' });
+      }
+      if (
+        validator.isEmpty(userInput.password) ||
+        !validator.isLength(userInput.password, { min: 6 })
+      ) {
+        errors.push({ msg: 'Password too short!' });
+      }
+      if (userInput.password !== userInput.passwordConfirmation) {
+        errors.push({ msg: 'Passwords do not match.'});
+      }
+      if (errors.length > 0) {
+        const error =  new Error('Invalid input.');
+          error.status = 422;
+          error.msg = 'Invalid input.';
+          error.data = errors;
+          throw(error);
+      }
+      // Handling data
+      return bcrypt.hash(userInput.password, 12)
+          .then(hash => {
+              const user = new User({
+                  email: userInput.email,
+                  password: hash
+              });
+              return user.save()
+                  .then(createdUser => {
+                      const { __v, ...createdUserData } = createdUser._doc;
+                      return {
+                        ...createdUserData,
+                        _id: createdUser._id.toString()
+                      };
+                  })
+                  .catch(err => {
+                      const error =  new Error(err);
+                      error.status = 400;
+                      error.msg = 'Unknown error occured.';
+                      throw(error);
+                  });
+          })
+          .catch(err => {
+              const error =  new Error(err);
+              error.status = 400;
+              error.msg = 'Unknown error occured.';
+              throw(error);
+          });
+      }
   },
   login: ({ userInput }, request) => {
     // Validation
